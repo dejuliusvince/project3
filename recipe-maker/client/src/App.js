7142891b04ea12d51936f55af2173e757595b032
vincedejulius <dejuliusvince@gmail.com>
2022-10-15 10:55:28 -0400

    boiler plate for client and server

---
+++ recipe-maker/client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     This will be our React Recipe Maker App!
+    </div>
+  );
+}
+
+export default App;
